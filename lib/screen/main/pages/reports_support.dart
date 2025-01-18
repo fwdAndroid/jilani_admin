e180824b14277c5df3f24d565d4e0eb092d40ead
fwdAndroid <fwdkaleem@gmail.com>
2025-01-18 23:53:33 +0500

    more tabs

--- lib/screen/main/pages/reports_support.dart
+++ lib/screen/main/pages/reports_support.dart
@@ -1,4 +1,9 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:jilani_admin/screen/tab/complains.dart';
+import 'package:jilani_admin/screen/tab/helpdesk.dart';
+import 'package:jilani_admin/utils/color.dart';
 
 class ReportsSupport extends StatefulWidget {
   const ReportsSupport({super.key});
@@ -10,6 +15,46 @@ class ReportsSupport extends StatefulWidget {
 class _ReportsSupportState extends State<ReportsSupport> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTabController(
+          length: 2,
+          child: Column(
+            children: <Widget>[
+              ButtonsTabBar(
+                labelSpacing: 60,
+                radius: 100,
+                contentCenter: true,
+                width: 120,
+                height: 50,
+                backgroundColor: mainColor,
+                unselectedLabelStyle: TextStyle(color: black),
+                labelStyle: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    fontFamily: GoogleFonts.inter().fontFamily),
+                tabs: [
+                  Tab(
+                    text: "Complains",
+                  ),
+                  Tab(
+                    text: "Help Desk",
+                  ),
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: <Widget>[
+                    Complains(),
+                    HelpDesk(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screen/tab/helpdesk.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HelpDesk extends StatefulWidget {
+  const HelpDesk({super.key});
+
+  @override
+  State<HelpDesk> createState() => _HelpDeskState();
+}
+
+class _HelpDeskState extends State<HelpDesk> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
